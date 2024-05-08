4e62b144cbc2380afd621ecc20e054f6c4a5c2f1
Emmanuel Oduor <eooduor31@gmail.com>
2024-05-08 18:09:11 +0300

    Version 1.0

--- app/src/main/java/com/example/qrcodescannerapplication/MainActivity.java
+++ app/src/main/java/com/example/qrcodescannerapplication/MainActivity.java
@@ -1,51 +1,86 @@
 package com.example.qrcodescannerapplication;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.MenuItem;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+import com.journeyapps.barcodescanner.CaptureActivity;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
+
 public class MainActivity extends AppCompatActivity {
-Button btnScan;
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle drawerToggle;
+    ActivityResultLauncher<ScanOptions> barLauncher;
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Get reference to the scan button
-        btnScan = (Button) findViewById(R.id.btnScan);
-        // Set click listener for the scan button
-        btnScan.setOnClickListener(v -> {
-                ScanCode();
-    });
-}
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.scan) {
+                    ScanCode();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        // Initialize the ActivityResultLauncher here
+        barLauncher = registerForActivityResult(new ScanContract(), result -> {
+            if (result.getContents() != null) {
+                // Display the scanned result in an alert dialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Result");
+                builder.setMessage(result.getContents());
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                }).show();
+            }
+        });
+    }
+
     private void ScanCode() {
         // Create scan options with desired settings
-        ScanOptions  options = new ScanOptions();
-        options.setPrompt("Volume up to flash on");// Set the prompt message
-        options.setBeepEnabled(true);// Enable beep sound on successful scan
-        options.setOrientationLocked(true);// Lock the orientation during scanning
-        options.setCaptureActivity(CaptureActivity.class);// Set the capture activity class to be used
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to flash on"); // Set the prompt message
+        options.setBeepEnabled(true); // Enable beep sound on successful scan
+        options.setOrientationLocked(true); // Lock the orientation during scanning
+        options.setCaptureActivity(CaptureActivity.class); // Set the capture activity class to be used
         // Launch the scan activity and handle the result
         barLauncher.launch(options);
     }
-    // Register a result handler for the scan activity
-    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(),result->{
-        if(result.getContents() != null)
-        {
-            // Display the scanned result in an alert dialog
-            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-            builder.setTitle("Result");
-            builder.setMessage(result.getContents());
-            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    dialogInterface.dismiss();
-                }
-            }).show();
-        }
-    });
 }
